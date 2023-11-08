f6993fd7ea5c5310dca03e2392b377d533c31f76
anatoly <anatoly.ripak@gmail.com>
2023-11-08 16:21:27 +0300

    Работа с реальным сканом в sun_model_SGD.py.

--- Tensor_flow/sun_model_SGD.py
+++ Tensor_flow/sun_model_SGD.py
@@ -1,5 +1,6 @@
 import os
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
 import numpy as np
@@ -30,18 +31,22 @@ def sun_with_noise(_x, _level=1.0, _edge_l=-90, _edge_r=90, _width_l=10, _width_
     return _sun + _noise
 
 
+def f_add(_x_batch, _level_mod, _edge_mod_l, _edge_mod_r, _width_mod_l, _width_mod_r):
+    a = tf.Variable(tf.zeros_like(_x_batch, dtype=float))
+    return a
+
+
 def model():
     _end = 30.0
     _step_x = 0.1
     _x_n = np.arange(-_end, _end + 0.1, _step_x)
-    _data = tf.math.log(sun_with_noise(tf.Variable(_x_n, dtype=float), 1.0, -19.0, 21.0, 2.0, 2.5) +
-                        flare(tf.Variable(_x_n, dtype=float), [1.5, 2.5, 4], [3.0, -5.0, 0], [1.0, 1.8, 1.5]) + 1.0)
+    _data = sun_with_noise(tf.Variable(_x_n, dtype=float), 1.0, -19.0, 21.0, 2.0, 2.5) + \
+            flare(tf.Variable(_x_n, dtype=float), [1.5, 2.5, 4], [3.0, -5.0, 0], [1.0, 1.8, 1.5]) + 1.0
     return _x_n, _data
 
 
 @tf.function
 def batch_train(_x_batch, _y_batch):
-
     _y_batch = data[n_batch * BATCH_SIZE: (n_batch + 1) * BATCH_SIZE]
     _x_batch = x_n[n_batch * BATCH_SIZE: (n_batch + 1) * BATCH_SIZE]
 
@@ -59,6 +64,44 @@ def batch_train(_x_batch, _y_batch):
                              height_mod1, pos_mod1, width1_mod1]))
 
     return d_loss, dp1, dp2, dp3, dp4, dp5, db, dh, dp, dw
+
+
+def sun_optimiser(_x_n, _data, _EPOCHS, _BATCH_SIZE, _learn_rate, _flag):
+    _opt = tf.optimizers.Adam(learning_rate=_learn_rate)  # EPOCHS = 80
+
+    _loss_dyn = np.array([])
+
+    _TOTAL_POINTS = len(x_n)
+    _num_steps = _TOTAL_POINTS // _BATCH_SIZE
+
+    for n in range(_EPOCHS):
+        for _n_batch in range(_num_steps):
+            _y_batch = _data[_n_batch * _BATCH_SIZE: (_n_batch + 1) * _BATCH_SIZE]
+            _x_batch = _x_n[_n_batch * _BATCH_SIZE: (_n_batch + 1) * _BATCH_SIZE]
+
+            with tf.GradientTape() as _t:
+                _f = flare(_x_batch, height_mod1, pos_mod1, width1_mod1) + b + \
+                     f_add(_x_batch, level_mod, edge_mod_l, edge_mod_r, width_mod_l, width_mod_r)
+                if _flag == 1:
+                    _f = quiet_sun(_x_batch, level_mod, edge_mod_l, edge_mod_r, width_mod_l, width_mod_r) + \
+                         flare(_x_batch, height_mod1, pos_mod1, width1_mod1) + b
+                _d_loss = tf.reduce_mean(tf.square(_y_batch - _f))
+
+            _dp1, _dp2, _dp3, _dp4, _dp5, _db, _dh, _dp, _dw = _t.gradient(_d_loss,
+                                                                           [level_mod, edge_mod_l, edge_mod_r,
+                                                                            width_mod_l,
+                                                                            width_mod_r, b,
+                                                                            height_mod1, pos_mod1, width1_mod1])
+            _opt.apply_gradients(zip([_dp1, _dp2, _dp3, _dp4, _dp5, _db, _dh, _dp, _dw],
+                                     [level_mod, edge_mod_l, edge_mod_r, width_mod_l, width_mod_r, b,
+                                      height_mod1, pos_mod1, width1_mod1]))
+            _loss_dyn = np.append(_loss_dyn, _d_loss.numpy())
+        _n_batch = 0
+
+    plt.plot(_loss_dyn)
+    plt.show()
+
+    return level_mod, edge_mod_l, edge_mod_r, width_mod_l, width_mod_r, b, height_mod1, pos_mod1, width1_mod1
 
 
 mod = 'n'
@@ -89,35 +132,8 @@ height_mod1 = tf.Variable([4000.0, 5000.0, 9000.0, 14000.0, 3000.0, 3000.0, 5000
 pos_mod1 = tf.Variable([161.0, 179.0, 191.0, 200.0, 222.0, 255.0, 266.0, 280], name='pos1')
 width1_mod1 = tf.Variable([7.0, 7.0, 7.0, 7.0, 7.0, 7.0, 7.0, 7.0], name='width1')
 
-opt = tf.optimizers.Adam(learning_rate=0.5)  # EPOCHS = 80
-
-EPOCHS = 100
-# learnin_rate = 3.0
-loss_dyn = np.array([])
-
-BATCH_SIZE = 225
-TOTAL_POINTS = len(x_n)
-num_steps = TOTAL_POINTS // BATCH_SIZE
-
-for n in range(EPOCHS):
-    for n_batch in range(num_steps):
-        y_batch = data[n_batch * BATCH_SIZE: (n_batch + 1) * BATCH_SIZE]
-        x_batch = x_n[n_batch * BATCH_SIZE: (n_batch + 1) * BATCH_SIZE]
-
-        with tf.GradientTape() as t:
-            f = quiet_sun(x_batch, level_mod, edge_mod_l, edge_mod_r, width_mod_l, width_mod_r) + \
-                flare(x_batch, height_mod1, pos_mod1, width1_mod1) + b
-            d_loss = tf.reduce_mean(tf.square(y_batch - f))
-
-        dp1, dp2, dp3, dp4, dp5, db, dh, dp, dw = t.gradient(d_loss,
-                                                              [level_mod, edge_mod_l, edge_mod_r, width_mod_l,
-                                                               width_mod_r, b,
-                                                               height_mod1, pos_mod1, width1_mod1])
-        opt.apply_gradients(zip([dp1, dp2, dp3, dp4, dp5, db, dh, dp, dw],
-                                [level_mod, edge_mod_l, edge_mod_r, width_mod_l, width_mod_r, b,
-                                 height_mod1, pos_mod1, width1_mod1]))
-        loss_dyn = np.append(loss_dyn, d_loss.numpy())
-    n_batch = 0
+level_mod, edge_mod_l, edge_mod_r, width_mod_l, width_mod_r, b, height_mod1, pos_mod1, width1_mod1 = \
+    sun_optimiser(x_n, data, 100, 225, 0.3, 1)
 
 sun_calc = quiet_sun(x_n, level_mod, edge_mod_l, edge_mod_r, width_mod_l, width_mod_r) + \
            flare(x_n, height_mod1, pos_mod1, width1_mod1) + b
@@ -132,44 +148,43 @@ axes.semilogy(x_n, data_n.numpy())
 axes.grid(True)
 plt.show()
 
-plt.plot(loss_dyn)
-plt.show()
-
 height_mod2 = tf.Variable(height_mod1, name='height')
 pos_mod2 = tf.Variable(pos_mod1, name='pos')
 width1_mod2 = tf.Variable(width1_mod1, name='width')
 
-opt = tf.optimizers.Adam(learning_rate=2.5)  # EPOCHS = 80
-
-loss_dyn = np.array([])
-EPOCHS = 300
-BATCH_SIZE = 450
-TOTAL_POINTS = len(x_n)
-num_steps = TOTAL_POINTS // BATCH_SIZE
-
-for n in range(EPOCHS):
-    loss = tf.Variable(0.0)
-    for n_batch in range(num_steps):
-        y_batch = data_n[n_batch * BATCH_SIZE: (n_batch + 1) * BATCH_SIZE]
-        x_batch = x_n[n_batch * BATCH_SIZE: (n_batch + 1) * BATCH_SIZE]
-        with tf.GradientTape() as t:
-            f = flare(x_batch, height_mod2, pos_mod2, width1_mod2) + b
-            d_loss = tf.reduce_mean(tf.square(y_batch - f))
-        loss.assign(d_loss)
-        loss_dyn = np.append(loss_dyn, loss.numpy())
-        dh1, dp1, dw1, db1 = t.gradient(d_loss, [height_mod2, pos_mod2, width1_mod2, b])
-        opt.apply_gradients(zip([dh1, dp1, dw1, db1],
-                                [height_mod2, pos_mod2, width1_mod2, b]))
-    n_batch = 0
-sun_calc1 = flare(x_n, height_mod2, pos_mod2, width1_mod2) + b
+level_mod, edge_mod_l, edge_mod_r, width_mod_l, width_mod_r, b, height_mod1, pos_mod1, width1_mod1 = \
+    sun_optimiser(x_n, data_n, 300, 450, 2.5, 0)
+# opt = tf.optimizers.Adam(learning_rate=2.5)  # EPOCHS = 80
+#
+# loss_dyn = np.array([])
+# EPOCHS = 300
+# BATCH_SIZE = 450
+# TOTAL_POINTS = len(x_n)
+# num_steps = TOTAL_POINTS // BATCH_SIZE
+#
+# for n in range(EPOCHS):
+#     loss = tf.Variable(0.0)
+#     for n_batch in range(num_steps):
+#         y_batch = data_n[n_batch * BATCH_SIZE: (n_batch + 1) * BATCH_SIZE]
+#         x_batch = x_n[n_batch * BATCH_SIZE: (n_batch + 1) * BATCH_SIZE]
+#         with tf.GradientTape() as t:
+#             f = flare(x_batch, height_mod2, pos_mod2, width1_mod2) + b
+#             d_loss = tf.reduce_mean(tf.square(y_batch - f))
+#         loss.assign(d_loss)
+#         loss_dyn = np.append(loss_dyn, loss.numpy())
+#         dh1, dp1, dw1, db1 = t.gradient(d_loss, [height_mod2, pos_mod2, width1_mod2, b])
+#         opt.apply_gradients(zip([dh1, dp1, dw1, db1],
+#                                 [height_mod2, pos_mod2, width1_mod2, b]))
+#     n_batch = 0
+sun_calc1 = flare(x_n, height_mod1, pos_mod1, width1_mod1) + b
 
 dat2 = dat - sun_calc1.numpy()
 
-print(b, height_mod2, pos_mod2, width1_mod2, sep='\n')
+print(b, height_mod1, pos_mod1, width1_mod1, sep='\n')
 plt.plot(x_n, sun_calc1.numpy())
 plt.plot(x_n, data_n.numpy())
 plt.plot(x_n, dat2)
 plt.show()
 
-plt.plot(loss_dyn)
-plt.show()
+# plt.plot(loss_dyn)
+# plt.show()
