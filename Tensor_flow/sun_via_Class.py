bcb4a35205c889ffd3f53e0af507994676f07cc2
SAOFastAcquisition <anatoly.ripak@gmail.com>
2023-11-20 19:12:54 +0300

    Работа с sun_via_Class.py.py

--- Tensor_flow/sun_via_Class.py
+++ Tensor_flow/sun_via_Class.py
@@ -22,14 +22,24 @@ class DenseNN(tf.Module):
             self.pos_f = tf.Variable([161.0, 179.0, 191.0, 200.0, 222.0, 255.0, 266.0, 280], dtype=float, name='pos_f')
             self.width_f = tf.Variable([7.0, 7.0, 7.0, 7.0, 7.0, 7.0, 7.0, 7.0], dtype=float, name='width_f')
 
+        # _y1 = (1 + tf.math.erf((_x - self.sun[1]) / self.sun[3])) / 2
+        # _y2 = -(1 + tf.math.erf((_x - self.sun[2]) / self.sun[4])) / 2
+        _y = self.sun_quiet(_x) + self.sun_spot(_x, self.height_f[1], self.pos_f[1], self.width_f[1])
+        return _y
+
+    def sun_quiet(self, _x):
         _y1 = (1 + tf.math.erf((_x - self.sun[1]) / self.sun[3])) / 2
         _y2 = -(1 + tf.math.erf((_x - self.sun[2]) / self.sun[4])) / 2
-        _y = self.sun[0] * (_y1 + _y2)
-        return _y
+        return self.sun[0] * (_y1 + _y2)
+
+    def sun_spot(self, _x, _height, _pos, _width):
+        _spot = _height * tf.exp(-0.5 * ((_x - _pos) / _width) ** 2)
+        return _spot
 
 
 a = DenseNN(1)
 s = a.trainable_variables
+print(s)
 print(a.fl_init)
 x = np.arange(140.0, 280.0 + 0.1, 0.2)
 y = a(x)
