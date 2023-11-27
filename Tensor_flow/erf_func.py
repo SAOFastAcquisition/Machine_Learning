d5c8026674f100f3bd7d3199a4ed332e1db7c812
SAOFastAcquisition <anatoly.ripak@gmail.com>
2023-11-27 07:22:55 +0300

    Разложение скана на спокойное Солнце и гауссианы erf_func.py

--- Tensor_flow/erf_func.py
+++ Tensor_flow/erf_func.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from scipy.signal import argrelextrema as agl
 from scipy import stats
 from pathlib import Path
 
@@ -26,8 +27,7 @@ def nGauss(_x, *_p):
     res = 0
     for i in range(n):
         res += Gauss(_x, _p[i * 3], _p[i * 3 + 1], _p[i * 3 + 2])
-    res += (quiet_sun(_x, _p[-5], _p[-4], _p[-3], _p[-2]) + _p[-1])
-    return res
+    return res + (quiet_sun(_x, _p[-5], _p[-4], _p[-3], _p[-2]) + _p[-1])
 
 
 def plotAllGauss(_x, _p):
@@ -51,91 +51,179 @@ def model():
     return x, _y
 
 
-# начальные приближения для трёх гауссианов
-ip0 = [5000., 160., 7.,
-       4000., 180., 7.,
-       7000., 190., 7.,
-       14000., 200., 7.,
-       4000.0, 225.8, 5.,
-       1000., 245., 7.,
-       3000., 256., 7.,
-       8000., 266., 5.,
-       2000., 280., 7.,
-       5000., 150., 280., 10.0,
-       500.]
+def initial_cond():
+    # начальные приближения для трёх гауссианов
+    _ip0 = [5000., 160., 7.,
+            4000., 180., 7.,
+            7000., 190., 7.,
+            14000., 200., 7.,
+            4000.0, 225.8, 5.,
+            50., 245., 7.,
+            1000., 256., 7.,
+            8000., 266., 5.,
+            2000., 280., 7.,
+            5000., 150., 285., 10.0,
+            500.]
 
-# верхняя граница
-top_limits = [20000, 170., 10.,
-              9000., 190., 18.,
-              17000., 200., 10.,
-              25000, 215., 10.,
-              9000., 235., 15.,
-              1800., 255., 10.,
-              5000., 260., 10.,
-              15000., 270., 10.,
-              9000., 285., 10,
-              9000., 160., 300., 30.,
-              1500.]
+    # верхняя граница
+    _top_limits = [20000, 170., 10.,
+                   4000., 190., 18.,
+                   17000., 200., 10.,
+                   25000, 215., 10.,
+                   9000., 235., 15.,
+                   100., 255., 10.,
+                   2000., 260., 10.,
+                   15000., 270., 10.,
+                   9000., 285., 10,
+                   9000., 160., 300., 30.,
+                   2000.]
 
-# нижняя граница
-bottom_limits = [0, 140., 0.,
-                 0., 170., 0.,
-                 00., 180., 1.,
-                 0, 200., 1.,
-                 0., 220., 1.,
-                 0., 245., 1.,
-                 0., 250., 1.,
-                 0., 260., 1.,
-                 0., 275., 1,
-                 0., 140., 280., 3.,
-                 0.]
+    # нижняя граница
+    _bottom_limits = [0, 140., 0.,
+                      0., 170., 0.,
+                      00., 180., 1.,
+                      0, 200., 1.,
+                      0., 220., 1.,
+                      0., 245., 1.,
+                      0., 250., 1.,
+                      0., 260., 1.,
+                      0., 275., 1,
+                      0., 140., 285., 3.,
+                      0.]
+    return _ip0, _bottom_limits, _top_limits
 
-# X, Y = model()  Tensor_flow/
 
-path1 = Path("2023-02-17_04+16" + '_scan_freq.npy')
-path2 = Path("2023-02-17_04+16" + '_time.npy')
-freq_mask = [1200, 1380, 1465, 1600, 1700, 2265, 2490, 2710, 2800, 2860]
+def initial_cond1():
+    # начальные приближения для трёх гауссианов
+    _ip0 = [1000., 143., 7.,
+            3000., 155., 7.,
+            1000., 166., 7.,
+            3000., 181., 7.,
+            2000., 188., 5.,
+            1000., 212., 7.,
+            1000., 220., 7.,
+            1000., 242., 7.,
+            4000., 254., 5.,
+            4000., 267., 7.,
+            5000., 130., 280., 10.0,
+            600.]
 
-Yw = np.load(path1, allow_pickle=True)[:, 170:620]
-X = np.load(path2, allow_pickle=True)[170:620]
+    # верхняя граница
+    _top_limits = [20000., 150., 15.,
+                   20000., 160., 18.,
+                   20000., 170., 15.,
+                   20000., 185., 15.,
+                   20000., 195., 15.,
+                   20000., 220., 15.,
+                   20000., 225., 15.,
+                   20000., 250., 15.,
+                   20000., 260., 15.,
+                   22000., 272., 15.,
+                   22000., 140., 290., 25.,
+                   6000.]
 
-initial_data = {'bottom_limits': bottom_limits,
-                'top_limits': top_limits,
-                'initial_cond': ip0}
-lp = len(ip0)
-row_labels = []
-for i in range((lp - 5) // 3):
-    row_labels += ['a' + str(i + 1), 'x0' + str(i + 1), 'w0' + str(i + 1)]
-row_labels += ['a0', 'xl', 'xr', 'ws', 'b0']
-df = pd.DataFrame(data=initial_data, index=row_labels)
-num = np.shape(Yw)[0]
-for i in range(num):
-    lc = num - 1 - i
-    Y = Yw[lc, :]
-    plt.plot(X, Y, 'r+', markersize=3)
+    # нижняя граница
+    _bottom_limits = [0, 135., 1.,
+                      0., 150., 1.,
+                      0., 160., 1.,
+                      0., 175., 1.,
+                      0., 180., 1.,
+                      0., 205., 1.,
+                      0., 215., 1.,
+                      0., 235., 1.,
+                      0., 250., 1.,
+                      0., 260., 1.,
+                      0., 125., 275., 3.,
+                      0.]
+    return _ip0, _bottom_limits, _top_limits
 
-    # интерполяция экспериментальных данных
-    p, cov = curve_fit(nGauss, X, Y, p0=ip0, bounds=(bottom_limits, top_limits))
-    df[str(freq_mask[lc])] = p
-    ip0 = p
-    print("Параметры грауссианов: ")
-    for pl in p:
-        print(pl)
 
-        # погрешность вычислений
-    print("Станд. отклонение: ", np.std(Y - nGauss(X, *p)))
-    slope, ic, r_value, p_value, std_err = \
-        stats.linregress(Y, nGauss(X, *p))
+def load_param():
+    return np.load("2023-02-17_04+16" + '_decomp.npy', allow_pickle=True)
 
-    # вывод графиков
-    plt.ylabel('Антенная температура, К')
-    plt.xlabel('Время, сек')
-    plt.text(100, 15000, 'R$^2$=' + '%.4f' % r_value ** 2)
-    plotAllGauss(X, p)
-    plt.plot(X, nGauss(X, *p), 'g')
-    plt.grid('both')
-    plt.savefig('result.png')
+
+def load_data(_path):
+    _data = np.load(_path, allow_pickle=True)
+    _y = _data[0][280:1330, 501]
+    _time = np.array(_data[2][280:1330]) * 8.3886e-3
+    # plt.plot(_time, _y)
     # plt.show()
+    return _y, _time
 
-np.save(Path("2023-02-17_04+16" + '_decomp'), df)
-pass
+
+def param_analise():
+    _param = load_param()
+    _freq_mask = [1200, 1380, 1465, 1600, 1700, 2265, 2490, 2710, 2800, 2860]
+    _arg = _freq_mask[-1::-1]
+    _x0 = _param[0:28:3, 3:].transpose()
+    plt.plot(_arg, _x0)
+    plt.grid("both")
+    plt.show()
+    pass
+
+
+if __name__ == '__main__':
+    # param = load_param()
+    # param_analise()
+    # X, Y = model()  Tensor_flow/
+    path1 = Path('Tensor_flow/2023-10-25_05-24_stocks.npy')
+    # path1 = Path("Tensor_flow/2023-02-17_04+16" + '_scan_freq.npy')
+
+    path2 = Path("Tensor_flow/2023-02-17_04+16" + '_time.npy')
+    freq_mask = [1200, 1380, 1465, 1600, 1700, 2265, 2490, 2710, 2800, 2860]
+
+    #               ***** Initial condition *****
+    ip0, bottom_limits, top_limits = initial_cond1()
+    load_data(path1)
+    Yw, X = load_data(path1)
+
+    # X = np.load(path2, allow_pickle=True)[170:620]
+
+    initial_data = {'bottom_limits': bottom_limits,
+                    'top_limits': top_limits,
+                    'initial_cond': ip0}
+    lp = len(ip0)
+    row_labels = []
+    for i in range((lp - 5) // 3):
+        row_labels += ['a' + str(i + 1), 'x0' + str(i + 1), 'w0' + str(i + 1)]
+    row_labels += ['a0', 'xl', 'xr', 'ws', 'b0']
+
+    #            ****** DataFrame for component parameters ******
+    df = pd.DataFrame(data=initial_data, index=row_labels)
+
+    # num = np.shape(Yw)
+    num = 1
+    for i in range(num):
+        # Параметры составляющих вычисляются от бОльших частот к меньшим
+        lc = num - 1 - i
+        Y1 = Yw
+        num_a = np.isfinite(Y1)
+        Y = Y1[num_a]
+        X = X[num_a]
+        plt.plot(X, Y, 'r+', markersize=3)
+
+        #               ****** интерполяция экспериментальных данных ******
+        p, cov = curve_fit(nGauss, X, Y, p0=ip0, bounds=(bottom_limits, top_limits))
+        df[str(freq_mask[lc])] = p
+        # ip0 = p
+        print("Параметры грауссианов: ")
+        for pl in p:
+            print(pl)
+
+        #               ****** погрешность вычислений ******
+        print("Станд. отклонение: ", np.std(Y - nGauss(X, *p)))
+        slope, ic, r_value, p_value, std_err = \
+            stats.linregress(Y, nGauss(X, *p))
+
+        #                   ****** вывод графиков ******
+        plt.ylabel('Антенная температура, К')
+        plt.xlabel('Время, сек')
+        plt.text(100, 15000, 'R$^2$=' + '%.4f' % r_value ** 2)
+        plotAllGauss(X, p)
+        plt.plot(X, nGauss(X, *p), 'g')
+        plt.grid('both')
+        plt.savefig('result.png')
+        plt.show()
+
+    np.save(Path("2023-02-17_04+16" + '_decomp'), df)
+    pass
